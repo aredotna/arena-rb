1253cce5d387d1b9f539da87be9ca1d5567b224e
dzucconi <damonzucconi@gmail.com>
2012-11-26 15:00:24 -0500

    Cleans up and adds some comment documentation

--- lib/arena/api.rb
+++ lib/arena/api.rb
@@ -6,30 +6,42 @@ require 'arena/search_results'
 
 module Arena
   module API
+    # Returns paginated global list of Channels
     def channels(options={})
       object_from_response(Arena::ChannelResults, :get, "/channels", options)
     end
 
+    # Returns a full Channel representation
     def channel(id, options={})
       object_from_response(Arena::Channel, :get, "/channels/#{id}", options)
     end
 
+    # Returns thumbnail representation (first 9 Blocks) for the given Channel
     def channel_thumb(id, options={})
       object_from_response(Arena::Channel, :get, "/channels/#{id}/thumb", options)
     end
 
+    # Returns a paginated list of all Channels connected to the given Channel
     def channel_channels(id, options={})
-      collection_from_response(Arena::Channel, :get, "/channels/#{id}/channels", options, "channels")
+      object_from_response(Arena::ChannelResults, :get, "/channels/#{id}/channels", options)
     end
 
+    # Returns a full Block representation
     def block(id, options={})
       object_from_response(Arena::Block, :get, "/blocks/#{id}", options)
     end
 
+    # Returns a User representation
     def user(id, options={})
       object_from_response(Arena::User, :get, "/users/#{id}", options)
     end
 
+    # Returns a paginated list of all Channels a User has access to
+    def user_channels(id, options={})
+      object_from_response(Arena::ChannelResults, :get, "/users/#{id}/channels", options)
+    end
+
+    # Returns a Search object with an Array for Channels, Blocks and Users
     def search(query, option={})
       object_from_response(Arena::SearchResults, :get, "/search?q=#{query}", options)
     end
