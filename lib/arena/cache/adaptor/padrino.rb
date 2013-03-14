82b8863d96f8ddcdffa507620309f218d4fd5d02
dzucconi <damonzucconi@gmail.com>
2013-03-14 18:00:03 -0400

    Implements (grotesque) Padrino adaptor

--- lib/arena/cache/adaptor/padrino.rb
+++ lib/arena/cache/adaptor/padrino.rb
@@ -5,7 +5,22 @@ module Arena
     module Adaptor
       class Padrino < Arena::Cache::Adaptor::Generic
         def self.method_missing(method, *args, &block)
-          # todo
+          padrino =
+            if !Arena.padrino_app_name.nil?
+              Arena.padrino_app_name.constantize
+            else
+              Padrino.mounted_apps.first.name.constantize
+            end
+
+          key = args.present? ? "arena_#{method}_#{args.first}" : "arena_#{method}"
+          cache = padrino.cache.get(key)
+
+          if cache.nil?
+            padrino.cache.set(key, Arena.send(method, id), expires_in: Arena.expires_in)
+            cache = padrino.cache.get(key)
+          end
+
+          cache
         end
       end
     end
