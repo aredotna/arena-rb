dbb48ca436d719380f94e8f02b2026e66daf9350
dzucconi <damonzucconi@gmail.com>
2013-03-14 18:50:08 -0400

    Rails is too kind

--- lib/arena/cache/adaptor/padrino.rb
+++ lib/arena/cache/adaptor/padrino.rb
@@ -7,11 +7,14 @@ module Arena
         def self.method_missing(method, *args, &block)
           client = Arena::Client.new
           app    = Arena.padrino_app_name.constantize || Padrino.mounted_apps.first.name.constantize
-          key    = args.present? ? "arena_#{method}_#{args.first}" : "arena_#{method}"
+          key    = !args.nil? ? "arena_#{method}_#{args.first}" : "arena_#{method}"
           cache  = padrino.cache.get(key)
 
           if cache.nil?
+            object = !args.nil? ? client.send(method, args.first) : client.send(method)
+
             padrino.cache.set(key, client.send(method, id), expires_in: Arena.expires_in)
+            
             cache = padrino.cache.get(key)
           end
 
