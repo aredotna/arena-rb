755e4abb9a3c64c384c1a98fbde5b07e85c0775a
dzucconi <damonzucconi@gmail.com>
2013-04-25 14:34:37 -0400

    Not sure when I merged caching in; not done; removing

--- lib/arena.rb
+++ lib/arena.rb
@@ -2,7 +2,6 @@ require "arena/version"
 require_relative "./arena/client"
 require_relative "./arena/default"
 require_relative "./arena/configurable"
-require_relative "./arena/cache"
 
 module Arena
   class << self
@@ -17,16 +16,6 @@ module Arena
         @client
       else
         @client = Arena::Client.new(options)
-      end
-    end
-
-  private
-
-    def method_missing(method, *args, &block)
-      if use_caching
-        Cache.send(method, *args, &block)
-      else
-        self.client.send(method, *args, &block)
       end
     end
   end
